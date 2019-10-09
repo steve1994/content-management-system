76c5962978bd77dd0a3439a514004383a8fb4d57
steve1994 <steve.harnadi@gmail.com>
2019-10-09 11:46:54 +0700

    Create Client CMS

---
+++ client/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
+
+router.get('/register', function(req, res) {
+    res.render('register');
+})
+
+module.exports = router;
---
+++ client/routes/landing.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('landing/index');
+});
+
+router.get('/data', function(req, res) {
+    res.render('landing/data');
+})
+
+router.get('/datadate', function(req,res) {
+    res.render('landing/datadate');
+})
+
+router.get('/map', function(req,res) {
+    res.render('landing/map');
+})
+
+module.exports = router;
