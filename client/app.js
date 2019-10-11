b8850414f5489d7d02e6efb73534ea2f69ea195e
steve1994 <steve.harnadi@gmail.com>
2019-10-11 13:34:24 +0700

    Finish Data.ejs (not yet tested)

--- client/app.js
+++ client/app.js
@@ -10,6 +10,8 @@ var cors = require('cors');
 
 var app = express();
 
+app.use(cors());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/landing', landingRouter);
