e13f3ba74289df1e4ab190ce87c926b59fbccb6a
steve1994 <steve.harnadi@gmail.com>
2019-10-14 19:09:53 +0700

    Temporarily Finish Bar, Pie, and Line charts

--- client/routes/index.js
+++ client/routes/index.js
@@ -5,6 +5,22 @@ router.get('/', function(req,res) {
     res.render('dashboard/index');
 })
 
+router.get('/line', function(req,res) {
+    res.render('dashboard/line');
+})
+
+router.get('/pie', function(req,res) {
+    res.render('dashboard/pie');
+})
+
+router.get('/bar', function(req,res) {
+    res.render('dashboard/bar');
+})
+
+router.get('/map', function(req,res) {
+    res.render('dashboard/map');
+})
+
 router.get('/login', function(req, res) {
     res.render('authentication/login');
 });
